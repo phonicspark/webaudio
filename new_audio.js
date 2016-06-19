097555b0a8c72d26ae86136ec0dcda4fcf4264dd
phonicspark <phonicspark@gmail.com>
2016-06-20 00:23:51 +0900

    2
    2

---
+++ new_audio.js
@@ -0,0 +1,44 @@
+/*new_audio_js*/
+var context;
+var bufferLoader;
+
+function loadAndPlay() {
+    try {
+        context = new AudioContext();
+    }
+    catch(e) {
+        alert("Web Audio API is not supported in this browser");
+    }
+
+    bufferLoader = new BufferLoader(
+        context,
+        [
+        "sounds/pedestrian2.mp3",
+        "sounds/urban area.mp3",
+        "sounds/road.mp3",
+        ],
+        finishedLoading
+    );
+
+    bufferLoader.load();
+}
+
+
+
+function finishedLoading(bufferList) {
+    // Create three sources and buffers
+    var kick = context.createBufferSource();
+    var snare = context.createBufferSource();
+    var hihat = context.createBufferSource();
+    kick.buffer = bufferList[0];
+    snare.buffer = bufferList[1];
+    hihat.buffer = bufferList[2];
+
+    kick.connect(context.destination);
+    snare.connect(context.destination);
+    hihat.connect(context.destination);
+	// Play them together
+    kick.start(0);
+    snare.start(0);
+    hihat.start(0);
+}
